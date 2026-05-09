437f97bf54575dbf9e78b61bf6016ba5cdd9b17e
Shishank <dshishank9@gmail.com>
2026-05-09 23:00:54 +0530

    updated variables

--- Variables.py
+++ Variables.py
@@ -15,3 +15,16 @@ print(type(x))
 name = "Shsihank"
 print(name) 
 print(z+name)
+
+#Built in Data Types
+"""
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
