5abfbd0480148d032c8b97bc2a83b71f751a05d0
Shishank <dshishank9@gmail.com>
2026-05-09 23:00:01 +0530

    updated variables

---
+++ Variables.py
@@ -0,0 +1,17 @@
+#python has no particular way to assign variables
+#it initializes a variable the moment you assign a value to it
+x = 5
+y = "Shishank"
+print(x)
+print(y)
+# it is not mandatory to initialize a variable but to do so 
+
+z = str(3)
+
+#to get the type of variable use type
+
+print(type(x))
+#to declare string use "name"
+name = "Shsihank"
+print(name) 
+print(z+name)
