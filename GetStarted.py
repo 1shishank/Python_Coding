2f4deb6269eee378cde9671e5892d267252469c5
Shishank <dshishank9@gmail.com>
2026-05-09 22:30:47 +0530

    Saving Python intro

---
+++ GetStarted.py
@@ -0,0 +1,16 @@
+#This is the first py file to check python version use python --version
+import sys
+
+print("Hello Python")
+# To print python version in system
+print(sys.version)
+# to run python file in termianl type python then py file name
+print("to print number 24",25)
+#you can add comments by using # or
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Heelo")
