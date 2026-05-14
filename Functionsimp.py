d7c75c5c145a5a5d82e464cf87a416724ff61970
Shishank <dshishank9@gmail.com>
2026-05-14 14:23:40 +0530

    functions

--- Functionsimp.py
+++ Functionsimp.py
@@ -9,3 +9,17 @@ def ftoc(b):
     return (b-32) *5/9
 
 print(ftoc(-2))
+
+def my_function(animal, name):
+  print("I have a", animal)
+  print("My", animal + "'s name is", name)
+
+my_function(animal = "dog", name = "Buddy")
+
+def my_function(country = "Norway"):
+  print("I am from", country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
