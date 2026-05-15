dabdbfc64d31bb9ab494b4ae9d8bcc6b083e7eb1
Shishank <dshishank9@gmail.com>
2026-05-15 16:05:42 +0530

    Functions decorators update

--- Functionsimp.py
+++ Functionsimp.py
@@ -23,3 +23,39 @@ my_function("Sweden")
 my_function("India")
 my_function()
 my_function("Brazil")
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+# *args calls a list as an argument in afunction
+# **kwargs calls it as a dictionary
+
+def my_function(username, **details):
+  print("Username:", username)
+  print("Additional details:")
+  for key, value in details.items():
+    print(" ", key + ":", value)
+
+my_function("emil123", age = 25, city = "Oslo", hobby = "coding")
+
+""" The scope of variable follow LEGB Rule 
+Local - Inside the current function
+Enclosing - Inside enclosing functions (from inner to outer)
+Global - At the top level of the module
+Built-in - In Python's built-in namespace """
+
+#A decorator is a function that takes another function as input and returns a new function.
+
+
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase
+def myfunction():
+  return "Hello Sally"
+
+print(myfunction())
