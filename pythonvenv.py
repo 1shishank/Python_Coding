ca462ca7b6203a8bc86b2ea0cc0a4ad5c2a50d9c
Shishank <dshishank9@gmail.com>
2026-05-19 15:33:54 +0530

    Environment

---
+++ pythonvenv.py
@@ -0,0 +1,19 @@
+""" 
+
+To create a new virtual environment 
+python -m venv myfirstproject
+
+to activate 
+myfirstproject\Scripts\activate
+
+to deactivate 
+deactivate
+
+
+to delete 
+rmdir /s /q myfirstproject
+
+the packages installed in the virtual environment are only there if you deativate it you cannot use it
+anymore
+
+"""
